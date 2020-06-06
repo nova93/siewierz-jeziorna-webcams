fa7a6fba62fe5e29b6395b77a0d492e8c6dc2815
Nova Sykst <novasykst@gmail.com>
2020-06-06 11:26:27 +0100

    CSP header

---
+++ script.js
@@ -0,0 +1,34 @@
+function openLarge(e) {
+  "grid" == rules[0].style.display
+    ? ((rules[0].style.display = "block"),
+      (rules[1].style.display = "none"),
+      (rules[1].style.width = "100vw"),
+      (rules[1].style.height = "100vh"),
+      (rules[1].style.objectFit = "contain"),
+      (e.style.display = "block"))
+    : ((rules[0].style.display = "grid"),
+      (rules[1].style.display = "block"),
+      (rules[1].style.width = "100%"),
+      (rules[1].style.height = "100%"),
+      (rules[1].style.objectFit = "cover"),
+      (e.style.display = ""));
+}
+function openLarge(e) {
+  "grid" == rules[0].style.display
+    ? ((rules[0].style.display = "block"),
+      (rules[1].style.display = "none"),
+      (rules[1].style.width = "100vw"),
+      (rules[1].style.height = "100vh"),
+      (rules[1].style.objectFit = "contain"),
+      (e.style.display = "block"))
+    : ((rules[0].style.display = "grid"),
+      (rules[1].style.display = "block"),
+      (rules[1].style.width = "100%"),
+      (rules[1].style.height = "100%"),
+      (rules[1].style.objectFit = "cover"),
+      (e.style.display = ""));
+}
+(sheet = document.styleSheets[0]),
+  (rules = sheet.cssRules || sheet.rules),
+  (sheet = document.styleSheets[0]),
+  (rules = sheet.cssRules || sheet.rules);
